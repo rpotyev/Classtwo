03c9912083c0e75c833f055df8212f6a7c6d2829
roman test <potyevromka@gmail.com>
2023-05-27 02:43:22 +0600

    commit 4

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+    public int calculate(int bodyWeight){
+        int height;
+        height = (int) (1.80 * 2); // рост указан в метрах
+
+        return height;
+
+    }
+
+}
